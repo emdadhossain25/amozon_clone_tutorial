200cc98d623e13deb7a390667da124ce3cfe474b
emdad <emdad@ergo-ventures.com>
2023-04-05 14:39:31 +0600

    inital setup for flutter app

---
+++ android/app/src/main/kotlin/com/example/amozon_clone_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amozon_clone_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:amozon_clone_tutorial/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amozon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Hello.'),
+        ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'click',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
